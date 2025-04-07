684a455a65e78e3ac1fd82b2ba53d16affc3bc80
徐瑞骏 <1687701765@qq.com>
2025-04-07 14:12:25 +0800

    添加Leaflet.MeasureControl插件用于测量距离

--- download.sh
+++ download.sh
@@ -1,3 +1,10 @@
 wget https://leafletjs-cdn.s3.amazonaws.com/content/leaflet/v1.9.4/leaflet.zip
 mkdir leaflet
 unzip leaflet.zip -d leaflet
+
+git submodule init
+
+cd Leaflet.draw
+npm install -g jake
+npm install
+jake build[3v,custom]
